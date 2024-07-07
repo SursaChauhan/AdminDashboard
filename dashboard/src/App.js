e0da1f6b68dfafd5ab85d833249a14203264230b
surendrasingh@Surendras-MacBook-Air.local <ss0622187@gmail.com>
2024-07-07 23:34:08 +0530

    file uplaoded to db

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -6,27 +6,10 @@ import { useState } from 'react';
 import axios from 'axios';
 
 function App() {
-  const[file,setFile] =useState(null);
-  const upload=() =>{
-    const formdata =new FormData();
-    console.log(file);
-  
-  
-    formdata.append('file',file);
-    console.log(formdata);
-    for (let pair of formdata.entries()) {
-      console.log(pair[0] + ": " + pair[1]);
-    }
-    axios.post('http://localhost:8080/api/upload',formdata).then(res =>{}).catch(er=>console.error(er))
-  }
-  
-
   return (
     <div className="App">
      
       <Appbar/>
-      {/* <input type='file' onChange={(e)=> setFile(e.target.files[0])}/>
-      <button onClick={upload}>upload</button> */}
     </div>
   );
 }
